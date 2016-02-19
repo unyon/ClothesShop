07901c93b266d79985715917ad9608c3f3e99908
unyon <michalloran@hotmail.com>
2016-02-19 19:59:22 +0000

    add drag down boxes for column fields with options hashes on model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,16 +3,48 @@ class Product < ActiveRecord::Base
 	belongs_to :admin
 	belongs_to :category
 
-	  Sizes = {
-    '1'      => 'one',
-    '2'      => 'two',
-    'three'  => '3',
-    'four'   => '4',
-    'five'   => '5',
-    'six'    => '6',
-    'Small'  => 'Small',
-    'Medium' => 'Medium',
-    'Large'  => 'Large'
-  }
+	Sizes = {
+		'0'      => '0',
+		'1'      => '1',
+		'2'      => '2',
+		'3'  	 => '3',
+		'4'   	 => '4',
+		'5'   	 => '5',
+		'6'    	 => '6',
+		'7'  	 => '7',
+		'8'   	 => '8',
+		'9'   	 => '9',
+		'10'     => '10',
+		'Small'  => 'Small',
+		'Medium' => 'Medium',
+		'Large'  => 'Large'
+	}
+
+	Kinds = {
+		'denim'  			=> 'denim',
+		'sweaters'  		=> 'sweaters',
+		'shirts & tops'  	=> 'tops',
+		'tees & more' 		=> 'tees',
+		'jackets & coats'   => 'jackets_coats',
+		'pants & shorts'    => 'pants_shorts',
+		'dresses'   		=> 'dresses',
+		'skirts'   			=> 'skirts',
+		'swim & beachwear'  => 'swim_beachwear',
+		'shoes & boots'   	=> 'shoes_boots',
+		'bags'   			=> 'bags',
+		'jewelry'   		=> 'jewelry'
+	}
+
+	Genders = {
+		'Woman'  	=> 'Woman',
+		'Men'  		=> 'Men',
+		'Boys'  	=> 'Boys', 
+		'Girls' 	=> 'Girls',
+		'Baby' 		=> 'Baby'
+	}
+
+
+
+
 end
 
